5d96dc1cbddc4b638b89bb6daf5a39b9abe3c67b
Nidhi Danayak <70342015+nidhidanayak@users.noreply.github.com>
2022-07-05 16:01:10 -0700

    Update main.py

--- NGCF/main.py
+++ NGCF/main.py
@@ -18,7 +18,7 @@ from time import time
 
 if __name__ == '__main__':
 
-    args.device = torch.device('cuda:' + str(args.gpu_id))
+    args.device = torch.device('cuda:0' + str(args.gpu_id))
     #args.device = torch.device('cuda:0')
     #args.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
