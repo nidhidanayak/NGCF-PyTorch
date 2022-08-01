a2432d9d9b3f8cd6421a8256f6ff951e7b643236
Nidhi Danayak <70342015+nidhidanayak@users.noreply.github.com>
2022-08-01 11:30:36 -0400

    Update main.py

--- NGCF/main.py
+++ NGCF/main.py
@@ -111,12 +111,12 @@ if __name__ == '__main__':
     hit = np.array(hit_loger)
     print(recs)
 
-    #best_rec_0 = max(recs[:])
-    #idx = list(recs).index(best_rec_0)
+#     best_rec_0 = max(recs[:, 0])
+#     idx = list(recs[:, 0]).index(best_rec_0)
 
-    final_perf = "Best Iter=[%d]@[%.1f]\trecall=[%s], precision=[%s], hit=[%s], ndcg=[%s]" % \
-                 (idx, time() - t0, '\t'.join(['%.5f' % r for r in recs[idx]]),
-                  '\t'.join(['%.5f' % r for r in pres[idx]]),
-                  '\t'.join(['%.5f' % r for r in hit[idx]]),
-                  '\t'.join(['%.5f' % r for r in ndcgs[idx]]))
-    print(final_perf)
+#     final_perf = "Best Iter=[%d]@[%.1f]\trecall=[%s], precision=[%s], hit=[%s], ndcg=[%s]" % \
+#                  (idx, time() - t0, '\t'.join(['%.5f' % r for r in recs[idx]]),
+#                   '\t'.join(['%.5f' % r for r in pres[idx]]),
+#                   '\t'.join(['%.5f' % r for r in hit[idx]]),
+#                   '\t'.join(['%.5f' % r for r in ndcgs[idx]]))
+#     print(final_perf)
